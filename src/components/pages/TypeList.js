362a3786129eb7d51a60babc43d858a44f03a8e0
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-11-24 13:59:32 +0200

    mount state data from pokemon API

--- src/components/pages/TypeList.js
+++ src/components/pages/TypeList.js
@@ -1,8 +1,34 @@
 import React from "react";
+import axios from 'axios';
 
 class TypeList extends React.Component{
+
+    state = {
+        data : {
+            count: 0,
+            next: '',
+            previous: '',
+            results: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`https://pokeapi.co/api/v2/type`)
+            .then(response => this.setState({data : response.data}))
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
     render() {
-        return <h1>First Type</h1>
+        return (
+            <React.Fragment>
+                <div>
+                    <ul>{this.state.data.results.map(result => <li>{result.name}</li>)}</ul>
+                </div>
+            </React.Fragment>
+        )
+
     }
 }
 
