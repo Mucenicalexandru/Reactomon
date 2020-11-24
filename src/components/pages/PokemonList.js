a2caf8677e317f43480255c405548b6640252900
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-11-24 14:04:12 +0200

    mount state data from pokemon API - pokemon names

--- src/components/pages/PokemonList.js
+++ src/components/pages/PokemonList.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class PokemonList extends React.Component{
+
+    state = {
+        data:{
+            count: 0,
+            next: '',
+            previous: '',
+            results: []
+        }
+
+    }
+
+    componentDidMount() {
+        axios.get(`https://pokeapi.co/api/v2/pokemon`)
+            .then(response => this.setState({data: response.data}))
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
     render() {
-        return <h1>First Pokemon</h1>
+        return <React.Fragment>
+            <div>
+                <ul>{this.state.data.results.map(result =>
+                    <li>
+                        {result.name}
+                    </li>
+                )}</ul>
+            </div>
+            </React.Fragment>
     }
 }
 
